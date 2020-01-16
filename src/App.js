3d5ce7825bd3497a3870b47b811c26a7c8c2e8d2
Adrian Markuszewski <adrian991999@wp.pl>
2020-01-16 13:56:58 +0100

    Delete additional lines

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import FreeTables from './components/views/FreeTable/FreeTable';
 import SeeWaiter from './components/views/SeeWaiter/SeeWaiter';
 import Kitchen from './components/views/Kitchen/Kitchen';
 import Home from './components/views/Home/Home';
-// import FreeTableEdit from './components/views/FreeTable/FreeTableEdit/FreeTableEdit';
-// import FreeTableEvent from'./components/views/FreeTable/FreeTableEvent/FreeTableEvent';
-// import FreeTableReservation from'./components/views/FreeTable/FreeTableReseration/FreeTableReservation';
-// import DetailsEvents from './components/views/DetailsEvents/DetailsEvents';
-// import DetailsReservation from './components/views/DetailsReservation/DetailsReservation';
-// import SeeWaiterClose from './components/views/SeeWaiter/SeeWaiterClose/SeeWaiterClose';
-import SeeWaiterEdit from './components/views/SeeWaiter/SeeWaiterEdit/SeeWaiterEdit';
-// import SeeWaiterNew from './components/views/SeeWaiter/SeeWaiterNew/SeeWaiterNew';
 import { StylesProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
@@ -42,18 +34,9 @@ function App() {
               <Route exact path={process.env.PUBLIC_URL + `/Dashboard`} component={Dashboard}/>
               <Route exact path={process.env.PUBLIC_URL + `/Login`} component={Login}/>
               <Route exact path={process.env.PUBLIC_URL + `/FreeTables`} component={FreeTables}/>
-              {/* <Route exact path={`${process.env.PUBLIC_URL}/FreeTables/DetailsEvents`} component={DetailsEvents}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/FreeTables/DetailsReservation`} component={DetailsReservation}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/FreeTables/Edit/:id`} component={FreeTableEvent}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/FreeTables/Event/:id`} component={FreeTableEdit}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/FreeTables/New/:id`} component={FreeTableReservation}/>
-
-          <Route exact path={`${process.env.PUBLIC_URL}/SeeWaiter/Close`} component={SeeWaiterClose}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/SeeWaiter/Edit`} component={SeeWaiterEdit}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/SeeWaiter/New`} component={SeeWaiterNew}/> */}
               <Route exact path={process.env.PUBLIC_URL+ `/SeeWaiter`} component={SeeWaiter}/>
               <Route exact path={process.env.PUBLIC_URL+ `/Kitchen`} component={Kitchen}/>
-              <Route exact path={`${process.env.PUBLIC_URL}/SeeWaiter/Edit`} component={SeeWaiterEdit}/>
+
             </Switch>
           </MainLayout>
         </ThemeProvider>
